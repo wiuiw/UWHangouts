e22942f39112ebf2cae25eb5f38be0241414fcfe
Harim Sanchez <harimsanchez@gmail.com>
2019-12-03 02:25:31 -0800

    Was able to get the local camera to appear for the user, still having some trouble getting remote users camera to appear

---
+++ public/js/app.js
@@ -0,0 +1,224 @@
+"use strict";
+
+//divs
+var divSelectRoom = $("#selectRoom");
+var divVCRoom = $("#vcRoom");
+var errors = $("#errors");
+var forms = $("#forms");
+var greeting = $("#greeting");
+
+//login info
+var usernameLI = $("#usernameLI");
+var passwordLI = $("#passwordLI");
+
+//sign up info
+var emailSU = $("#emailSU");
+var usernameSU = $("#usernameSU");
+var passwordSU = $("#passwordSU");
+
+//roomnumber and submitting info with login 
+//or sign up info
+var inputRoomName = $("#roomName");
+var submitLIRoom = $("#goRoomLI");
+var submitSURoom = $("#goRoomSU");
+
+//videos
+var localVideo = document.getElementById("localVideo");
+var remoteVideo = document.getElementById("remoteVideo");
+
+var roomName;
+var localStream;
+var remoteStream;
+var rtcPeerConnection;
+
+//STUN Servers
+var iceServers = {
+    'iceServers': [
+        { 'urls': 'stun:stun.services.mozilla.com' },
+        { 'urls': 'stun:stun.l.google.com:19302' }
+    ]
+}
+var streamConstraints = {audio:true, video:true};
+var isCaller;
+
+//Socket.io server
+var socket = io();
+    
+//onclick for submitting Log in for a user into a room
+submitLIRoom.click(function (){
+    if(inputRoomName.val() === ""){
+        errors.append("<h2>Please type a room name</h2>");
+    } else if(usernameLI.val() === "" || passwordLI.val() === ""){
+        errors.append("<h2>Please provide all user login info</h2>");
+    } else {
+
+        roomName = inputRoomName.val();
+
+        greeting.css("display", "block");
+        greeting.append("<h2>Welcome to room " + inputRoomName.val() + ", " + usernameLI.val());
+
+        //check if log in info exists in the users database
+        
+        //if user exists create the room if it doesnt exist or create it
+        //otherwise
+
+        //join the room as that user
+
+        //make the vc div visible and hide the fields to log in or signup
+
+        socket.emit("create or join", roomName); //send message to socket server
+
+        forms.css("display", "none");
+        divVCRoom.css("display", "block");
+
+
+    }
+})
+
+//onclick for submitting sign up for a user into a room
+submitSURoom.click(function (){
+    if(inputRoomName.val() === ""){
+        errors.append("<h2>Please type a room name</h2>");
+    } else if(emailSU.val() === "" || usernameSU.val() === "" || passwordSU.val() === ""){
+        errors.append("<h2>Please provide all user sign up info</h2>");
+    } else {
+
+        roomName = inputRoomName.val();
+
+        greeting.css("display", "block");
+        greeting.append("<h2>Welcome to room " + inputRoomName.val() + ", " + usernameLI.val());
+
+
+        //check if signup info exists in the users database to not have duplicate info
+
+        //create the user if the info is unique and add them to the database
+        
+        //if user exists create the room if it doesnt exist or create it
+        //otherwise
+
+        //join the room as that user
+
+        //make the vc div visible and hide the fields to log in or signup
+
+        socket.emit("create or join", roomName); //send message to socket server
+
+        forms.css("display", "none");
+        divVCRoom.css("display", "block");
+
+    }
+})
+
+//sends signal when socket server is created
+socket.on('created', function (room) {
+    navigator.mediaDevices.getUserMedia(streamConstraints).then(function (stream) {
+        localStream = stream;
+        localVideo.srcObject = stream;
+        isCaller = true;
+    }).catch(function (err) {
+        console.log('An error ocurred when accessing media devices', err);
+    });
+});
+
+socket.on('joined', function (room) {
+    navigator.mediaDevices.getUserMedia(streamConstraints).then(function (stream) {
+        localStream = stream;
+        localVideo.srcObject = stream;
+        socket.emit('ready', roomNumber);
+    }).catch(function (err) {
+        console.log('An error ocurred when accessing media devices', err);
+    });
+});
+
+//sends signal when server is ready
+socket.on("ready", function(){
+    if(isCaller){
+        //create rtc PTP connection
+        rtcPeerConnection = new RTCPeerConnection(iceServers);
+
+        //event listeners
+        rtcPeerConnection.onicecandidate = onIceCandidate;
+        rtcPeerConnection.ontrack = onAddStream;
+        rtcPeerConnection.addTrack(localStream.getTracks()[0], localStream);
+        rtcPeerConnection.addTrack(localStream.getTracks()[1], localStream);
+
+        rtcPeerConnection.createOffer()
+            .then(sessionDescription => {
+                rtcPeerConnection.setLocalDescription(sessionDescription)
+                socket.emit("offer", {
+                    type:"offer",
+                    sdp:sessionDescription,
+                    room:roomName
+                })
+            }).catch(error => {
+                console.log(error);
+            });
+
+    }
+});
+
+//when servers send out an offer
+socket.on("offer", function(event){
+    if(!isCaller){
+        //prepare rtc PTP connection
+        rtcPeerConnection = new RTCPeerConnection(iceServers);
+
+        //event listeners
+        rtcPeerConnection.onicecandidate = onIceCandidate;
+        rtcPeerConnection.ontrack = onAddStream;
+
+        rtcPeerConnection.addTrack(localStream.getTracks()[0], localStream);
+        rtcPeerConnection.addTrack(localStream.getTracks()[1], localStream);
+        rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(event));
+
+        rtcPeerConnection.createAnswer()
+            .then(sessionDescription => {
+                rtcPeerConnection.setLocalDescription(sessionDescription)
+                socket.emit("answer", {
+                    type:"answer",
+                    sdp:sessionDescription,
+                    room:roomName
+                })
+            }).catch(error => {
+                console.log(error);
+            });
+
+
+    }
+});
+
+//when servers send out an answer
+socket.on("answer", function(event){
+    //store as remote description
+    rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(event));
+});
+
+//when servers send out a candidate
+socket.on("candidate", function(event){
+    //create candidate object
+    var candidate = new RTCIceCandidate({
+        sdpMLineIndex: event.label,
+        candidate: event.candidate
+    });
+    //store candidate
+    rtcPeerConnection.addIceCandidate(candidate);
+});
+
+//helps a user receive the video and audio of another user
+function onAddStream(event){
+    remoteVideo.srcObject = event.streams[0];
+    remoteStream = event.stream;
+}
+
+//listener for the p2p connection that sends a candidate message to the server
+function onIceCandidate(event){
+    if(event.candidate){
+        console.log("sending ice candidate");
+        socket.emit("candidate",{
+            type:"candidate",
+            label:event.candidate.sdpMLineIndex,
+            id:event.candidate.sdpMid,
+            candidate:event.candidate.candidate,
+            room: roomName
+        });
+    }
+}
--- server.js
+++ server.js
@@ -1,20 +1,62 @@
 "use strict";
 
 const express = require("express");
-
 const app = express();
-const port = 80;
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+const port = process.env.PORT || 3000;
 
 //clients folder as root
-app.use(express.static("clients"));
+app.use(express.static("public"));
 
-//provide access to node_modules folder from the client
-app.use('/scripts', express.static(`${__dirname}/node_modules/`));
+//signaling handlers
+io.on("connection", function(socket){
 
-//use index.html for traffic
-app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
+    console.log("a user has connected");
 
-app.listen(port, () => {
+    socket.on("create or join", function(room){
+
+        console.log("create or join to room ", room);
+
+        //check for number of users in a room
+        var myRoom = io.sockets.adapter.rooms[room] || {length:0};
+        var numClients = myRoom.length;
+        console.log(room, " has ", numClients, " users");
+
+
+        //there are no users in that room
+        if(numClients == 0){
+            socket.join(room);
+            socket.emit("created", room);
+        }else if(numClients == 1){ //one user in the room
+            socket.join(room);
+            socket.emit("joined", room);
+        }else{ //two users already in the room, cant allow more users
+            socket.emit("full", room);
+        }
+    });
+    
+    //relay only handlers
+    socket.on("ready", function(room){
+        socket.broadcast.to(room).emit("ready");
+    });
+
+    socket.on("candidate", function(event){
+        socket.broadcast.to(event.room).emit("candidate", event);
+    });
+
+    socket.on("offer", function(event){
+        socket.broadcast.to(event.room).emit("offer", event.sdp);
+    });
+
+    socket.on("answer", function(event){
+        socket.broadcast.to(event.room).emit("answer", event.sdp);
+    });
+
+
+});
+
+http.listen(port, () => {
     console.info("listening on %d", port)
-})
+});
 
